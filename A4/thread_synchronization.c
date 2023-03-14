af5e52c8dc98ba1eab363aed0ff5a615f73d6c04
Rhea Sharma <77135078+Rhea-Sharma@users.noreply.github.com>
2023-03-14 01:01:33 -0400

    Update thread_synchronization.c

--- A4/thread_synchronization.c
+++ A4/thread_synchronization.c
@@ -1,5 +1,5 @@
 /**
-CP386 A4 Q1 (stack.c)
+CP386 A4 Q2 (thread_synchronization.c)
 Author: Rhea Sharma & Mobina Tooranisama
 ID: 200576620 & 200296720
 Email: shar7662@mylaurier.ca & toor6720@mylaurier.ca
